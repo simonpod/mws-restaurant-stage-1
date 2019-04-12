c3645452ea39e72e14b155c4e4263d93ce344ec3
simonpod <s.podboraczynski@gmail.com>
2019-04-12 02:01:32 +0100

    feat/sw starter code

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+// service worker installation code source https://developers.google.com/web/fundamentals/primers/service-workers/
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
+
+
 let restaurants,
   neighborhoods,
   cuisines
