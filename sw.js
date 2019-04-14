f7d7248270ed34546ecd4ad179100c4c4e60e7c1
simonpod <s.podboraczynski@gmail.com>
2019-04-14 04:08:52 +0100

    feat/working cache

--- sw.js
+++ sw.js
@@ -1,3 +1,48 @@
-self.addEventListener('fetch', function () {
-	console.log(event.request);
-})
+//array of cached resources
+
+const resourceCache = [
+	'/',
+	'/index.html',
+	'/favicon.ico',
+	'/restaurant.html',
+	'/css/styles.css',
+	'/data/restaurants.json',
+	'/js/dbhelper.js',
+	'/js/main.js',
+	'/js/restaurant_info.js',
+	'/img/1.jpg',
+	'/img/2.jpg',
+	'/img/3.jpg',
+	'/img/4.jpg',
+	'/img/5.jpg',
+	'/img/6.jpg',
+	'/img/7.jpg',
+	'/img/8.jpg',
+	'/img/9.jpg',
+	'/img/10.jpg'
+
+];
+
+
+//stores site files in cache
+self.addEventListener('install', function(event) {
+	event.waitUntil(
+		caches.open('restaurant-cache').then(function(cache){
+			return cache.addAll(resourceCache);
+			console.log(cache);
+		})
+		);
+});
+
+
+//gets the files for offline view or logs new ones to the console
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+
+
